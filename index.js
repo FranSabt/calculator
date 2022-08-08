6ecb7611e3ec8c9d2c702a8393d2acaa64fa4962
FranSabt <hsabatino1@gmail.com>
2022-08-07 22:31:52 -0400

    logic is broken

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const sum = (num1, num2) =>{
-    console.log(num1 + num2
-        );
+    return num1 + num2
 } 
 
 
@@ -48,16 +47,22 @@ const calculator = {
         this.actualDisplay += val;
         document.getElementById('actual-num').innerHTML = calculator.actualDisplay;
     },
-    callOperator : function(val){
+    callOperator : function(val){ // I need to get out the parts that execute the operations
         if(this.operator = true){
-            operate(this.prevDisplay, this.oldOperator, this.actualDisplay);
+            let r = operate(this.prevDisplay, this.oldOperator, this.actualDisplay);
+            console.log(r);
+            !this.operator;
         }
         this.prevDisplay = this.actualDisplay;
         document.getElementById('previous-num').innerHTML = this.prevDisplay + ` ${val}`;
         this.actualDisplay = ''
-        this.operator = true;
         this.oldOperator = val;
     }
 }
 document.getElementById("previous-num").innerHTML = 0;
 document.getElementById('actual-num').innerHTML = 0;
+
+
+/* el problema es la secuencia de ejecucion con callOperator
+creo que necesitare colocar aparte la asignacion del operador 
+y luego hacer el llamado a las funciones de operacio */
